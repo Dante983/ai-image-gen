8a2ce93fa7f55fe016cb9699b5f935c25621641c
nikola <savicn209@gmail.com>
2023-06-03 13:40:34 +0200

    envb key bla bla

--- public/js/main.js
+++ public/js/main.js
@@ -1,60 +1,59 @@
 function onSubmit(e) {
     e.preventDefault();
-
+  
     document.querySelector('.msg').textContent = '';
     document.querySelector('#image').src = '';
-
+  
     const prompt = document.querySelector('#prompt').value;
     const size = document.querySelector('#size').value;
-
+  
     if (prompt === '') {
-        alert('please add some text');
-        return;
+      alert('Please add some text');
+      return;
     }
-
+  
     generateImageRequest(prompt, size);
-}
-
-async function generateImageRequest(prompt, size) {
+  }
+  
+  async function generateImageRequest(prompt, size) {
     try {
-        showSpinner();
-
-        const response = await fetch('/openai/generateimage', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application-json'
-            },
-            body: JSON.stringify({
-                prompt,
-                size
-            })
-        });
-        console.log(response);
-        if(!response.ok) {
-            removeSpinner();
-            throw new Error('That image could not be generated');
-        }
-
-        const data = await response.json();
-        
-        const imageUrl = data.data;
-
-        document.querySelector('#image').src = imageUrl;
-
+      showSpinner();
+  
+      const response = await fetch('/openai/generateimage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt,
+          size,
+        }),
+      });
+  
+      if (!response.ok) {
         removeSpinner();
+        throw new Error('That image could not be generated');
+      }
+  
+      const data = await response.json();
+      // console.log(data);
+  
+      const imageUrl = data.data;
+  
+      document.querySelector('#image').src = imageUrl;
+  
+      removeSpinner();
     } catch (error) {
-        document.querySelector('.msg').textContent = error;
+      document.querySelector('.msg').textContent = error;
     }
-
-}
-
-function showSpinner() {
+  }
+  
+  function showSpinner() {
     document.querySelector('.spinner').classList.add('show');
-}
-
-function removeSpinner() {
+  }
+  
+  function removeSpinner() {
     document.querySelector('.spinner').classList.remove('show');
-}
-
-document.querySelector('#image-form').addEventListener('submit', onSubmit);
-
+  }
+  
+  document.querySelector('#image-form').addEventListener('submit', onSubmit);
